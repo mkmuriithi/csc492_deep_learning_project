a066d0d565d72b05d8c40ae3bd613c4778646de9
Matthew Muriithi <muriithimatthew@gmail.com>
2022-03-09 00:42:11 -0500

    Defined new stocks list as of Mar 9, updated code for pulling all historic data
    A new list of all stocks available in the NYSE has been added, which contains all stocks that are available as of March 9th 2022.
    It does not matter if stocks that are no longer available are missing for the dataset as we won't be able to live trade those stocks, so it doens't make sense to use them.
    The new stock list is extracted from data downloaded directly from the relevant exchange, so it is most accurate with what stocks are available, and the download does not fail as with yfinance
    
    A new data source will be used (IEX Cloud). In Brainstorm.ipynb there is some code for pulling ALL historic data for a single stock, tested on facebook stock.
    Further fields will be added. IEX Cloud has a rate limit, so code to pull all historic data possible of all stocks in NYSE will be added (then from there we can use python to filter for specific dates).
    This will allow us to move past any issues of stocks that would not pull, or having to rewrite code for pulling data everytime.

--- algo_repo/utils.py
+++ algo_repo/utils.py
@@ -1,6 +1,8 @@
 import yfinance as yf
 import pandas as pd
 import numpy as np
+import requests
+import matplotlib.pyplot as plt
 
 
 def get_list_of_tickers():
@@ -60,3 +62,44 @@ def get_master_dataset(tickers_list=[], timestart="2007-01-01", timeend="2021-12
         threads=True,
     )
     return master_dataset
+
+
+def get_historic_data(symbol):
+    ticker = symbol
+    iex_api_key = 'Tsk_30a2677082d54c7b8697675d84baf94b'
+    api_url = f'https://sandbox.iexapis.com/stable/stock/{ticker}/chart/max?token={iex_api_key}'
+    df = requests.get(api_url).json()
+
+    date = []
+    open = []
+    high = []
+    low = []
+    close = []
+
+    for i in range(len(df)):
+        date.append(df[i]['date'])
+        open.append(df[i]['open'])
+        high.append(df[i]['high'])
+        low.append(df[i]['low'])
+        close.append(df[i]['close'])
+
+    date_df = pd.DataFrame(date).rename(columns={0: 'date'})
+    open_df = pd.DataFrame(open).rename(columns={0: 'open'})
+    open_df = pd.DataFrame(open).rename(columns={0: 'open'})
+    high_df = pd.DataFrame(high).rename(columns={0: 'high'})
+    low_df = pd.DataFrame(low).rename(columns={0: 'low'})
+    close_df = pd.DataFrame(close).rename(columns={0: 'close'})
+
+    frames = [date_df, open_df, high_df, low_df, close_df]
+    df = pd.concat(frames, axis=1, join='inner')
+    df = df.set_index('date')
+
+    df['open'].plot()
+    plt.title('{} Historical Prices'.format(ticker), fontsize=18)
+    plt.xlabel('Date', fontsize=14)
+    plt.ylabel('Stock Price', fontsize=14)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.show()
+
+    return df
