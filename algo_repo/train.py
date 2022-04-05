bf24c97e098b76efa6f2f300b3c63c77f0465630
Hazelstorm <sloanchochinov@gmail.com>
2022-04-04 21:28:11 -0400

    Added function to plot model predictions

--- algo_repo/train.py
+++ algo_repo/train.py
@@ -83,8 +83,8 @@ class transf_params:
     lr = 0.01
 
 
-def train(model, data, optimizer='adam', batch_size=8, learning_rate=1e-8, momentum=0.9, num_epochs=25,
-          weight_decay=0.5):
+def train(model, data, optimizer='adam', batch_size=8, learning_rate=1e-7, num_epochs=10,
+          weight_decay=0.1):
     # create training, valid and test sets of StockDataset type data
     train_custom, valid_custom, test_custom = split_data(data, window=60, minmax=True)
     # normalize data
@@ -103,9 +103,11 @@ def train(model, data, optimizer='adam', batch_size=8, learning_rate=1e-8, momen
     criterion = torch.nn.MSELoss(reduction='mean')
     iters, train_losses, val_losses, baseline_losses =  [], [], [], []
     # train
+    
+    n = 0
     for epoch in range(0, num_epochs):
         print(f'Epoch {epoch} training beginning...')
-        for n, data in enumerate(train_dataloader):
+        for data in train_dataloader:
             X, y, X_baseline, y_baseline = data
             mask = torch.zeros(X.shape[1], X.shape[1])
             if torch.cuda.is_available():
@@ -122,42 +124,59 @@ def train(model, data, optimizer='adam', batch_size=8, learning_rate=1e-8, momen
             optimizer.zero_grad()
             train_loss = loss.item() # save training loss
             
-            #annotate for evaluation
-            model.eval()
-            val_loss = []
-            with torch.no_grad():
-                for data in val_dataloader:
-                    X, y, X_baseline, y_baseline = data
-                    mask = torch.zeros(X.shape[1], X.shape[1])
-                    if torch.cuda.is_available():
-                        X = X.cuda()
-                        y = y.cuda()
-                        mask = mask.cuda()
-                        
-                    out = model(X, mask)
-                    loss = criterion(out, y)
-                    val_loss.append(loss.item()) # save validation loss
-                val_loss = np.mean(val_loss) # mean reduction
+            if (n % 10 == 0):
+                #annotate for evaluation
+                model.eval()
+                train_loss = []
+                val_loss = []
+                with torch.no_grad():
                     
-            #save current training info
-            iters.append(n)
-            train_losses.append(train_loss/batch_size) #avearge loss
-            val_losses.append(val_loss/batch_size)
-            baseline_losses.append(get_last_price_close_mse(y_baseline))
-            #train_acc.append(get_accuracy(model, train_custom, train=True))
-            #val_acc.append`(get_accuracy(model, valid_custom, train=False))
-            #train_losses.append(loss.item())  # average loss
-            if (n % 20 == 0):
-                print(f'Iteration: {n}, Train Loss: {train_loss}, Val Loss: {val_loss}')
+                    for data in train_dataloader:
+                        X, y, X_baseline, y_baseline = data
+                        mask = torch.zeros(X.shape[1], X.shape[1])
+                        if torch.cuda.is_available():
+                            X = X.cuda()
+                            y = y.cuda()
+                            mask = mask.cuda()
+                            
+                        out = model(X, mask)
+                        loss = criterion(out, y)
+                        train_loss.append(loss.item()) # save validation loss
+                    
+                    for data in val_dataloader:
+                        X, y, X_baseline, y_baseline = data
+                        mask = torch.zeros(X.shape[1], X.shape[1])
+                        if torch.cuda.is_available():
+                            X = X.cuda()
+                            y = y.cuda()
+                            mask = mask.cuda()
+                            
+                        out = model(X, mask)
+                        loss = criterion(out, y)
+                        val_loss.append(loss.item()) # save validation loss
+                        
+                    val_loss = np.mean(val_loss) # mean reduction
+                    train_loss = np.mean(train_loss)
+                        
+                #save current training info
+                iters.append(n)
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                baseline_losses.append(get_last_price_close_mse(y_baseline))
+                #train_acc.append(get_accuracy(model, train_custom, train=True))
+                #val_acc.append`(get_accuracy(model, valid_custom, train=False))
+                #train_losses.append(loss.item())  # average loss
+                print(f'Iteration: {n}, Train Loss: {train_losses[-1]}, Val Loss: {val_losses[-1]}')
+            n += 1
 
             # predict validation
     print(f'Final Training Loss: {train_losses[-1]}')
     # print(f'Final Validation Loss {val_losses[-1]}')
     # graph loss
-    plt.title("Learning Loss")
-    plt.plot(train_losses, label='Train')
-    plt.plot(val_losses, label='Validation')
-    plt.plot(baseline_losses, label='baseline')
+    plt.title(f"Training Curve (lr={learning_rate}, wd={weight_decay})")
+    plt.plot(iters, train_losses, label='Train')
+    plt.plot(iters, val_losses, label='Validation')
+    plt.plot(iters, baseline_losses, label='baseline')
     plt.legend()
     plt.xlabel("Iterations")
     plt.ylabel("Loss")
@@ -165,7 +184,7 @@ def train(model, data, optimizer='adam', batch_size=8, learning_rate=1e-8, momen
     plt.show()
 
 
-    return train_losses, val_losses, baseline_losses
+    return train_losses, val_losses, baseline_losses, iters
 
 
 def get_last_price_close_mse(y):
@@ -189,6 +208,79 @@ def get_last_price_close_mse(y):
         avg_mse += mse
     avg_mse = avg_mse / num_batches
     return avg_mse
+    
+def save_params(model):
+    torch.save(model.state_dict, "model.pt")
+    
+def load_params():
+    return torch.load("model.pt")
+    
+def plot_predictions(model, data):
+    train_custom, valid_custom, test_custom = split_data(data, window=60, minmax=True)
+    train_dataloader = DataLoader(train_custom, batch_size=1, shuffle=True)
+    val_dataloader = DataLoader(valid_custom, batch_size=1, shuffle=True)
+    
+    criterion = torch.nn.MSELoss(reduction='mean')
+    
+    train_truth=[]
+    train_pred=[]
+    train_loss=0
+    val_truth=[]
+    val_pred=[]
+    val_loss=0
+    model.eval()
+    
+    with torch.no_grad():
+        
+        for data in val_dataloader:
+            X, y, X_baseline, y_baseline = data
+            mask = torch.zeros(X.shape[1], X.shape[1])
+            if torch.cuda.is_available():
+                X = X.cuda()
+                y = y.cuda()
+                mask = mask.cuda()
+                
+            # add ground-truth and prediction
+            pred = model(X, mask)
+            val_loss += criterion(pred, y).item()
+            val_truth.append(y.item())
+            val_pred.append(pred.item())
+            
+        val_loss /= len(val_dataloader)
+            
+        for data in train_dataloader:
+            X, y, X_baseline, y_baseline = data
+            mask = torch.zeros(X.shape[1], X.shape[1])
+            if torch.cuda.is_available():
+                X = X.cuda()
+                y = y.cuda()
+                mask = mask.cuda()
+                
+            # add ground-truth and prediction
+            pred = model(X, mask)
+            train_loss += criterion(pred, y).item()
+            train_truth.append(y.item())
+            train_pred.append(pred.item())
+            
+        train_loss /= len(train_dataloader)
+            
+    print(f"Training Loss: {train_loss}")
+    plt.title(f"Model Prediction vs Ground Truth: Training Data")
+    plt.plot(train_truth, label="Ground Truth")
+    plt.plot(train_pred, label="Model Prediction")
+    plt.legend()
+    plt.xlabel("Time (Days)")
+    plt.ylabel("Normalized Closing Price")
+    plt.show()
+        
+    print(f"Validation Loss: {val_loss}")
+    plt.title(f"Model Prediction vs Ground Truth: Validation Data")
+    plt.plot(val_truth, label="Ground Truth")
+    plt.plot(val_pred, label="Model Prediction")
+    plt.legend()
+    plt.xlabel("Time (Days)")
+    plt.ylabel("Normalized Closing Price")
+    plt.show()
 
 if __name__ == '__main__':
 
@@ -223,4 +315,5 @@ if __name__ == '__main__':
     model = TransformerModel(transf_params)
     if torch.cuda.is_available():
         model = model.cuda()
-    train_losses, val_losses, baseline_losses = train(model, data)
+    train_losses, val_losses, baseline_losses, iters = train(model, data)
+        
